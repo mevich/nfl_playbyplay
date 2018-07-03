4edeb37bdae2601ddebbdeb382883e5a7d2b3cfe
mevich <vikram.medabalimi@gmail.com>
2018-07-03 12:30:30 -0700

    Update with base HTML and additional URLs

--- app.py
+++ app.py
@@ -19,26 +19,38 @@ def after_request(response):
     return response
 
 
-@app.route('/')
-def get_index():
-    return render_template('index.html')
+@app.route("/")
+def get_all_seasons():
+    query_object = Nflpbp.select(Nflpbp.season).distinct().dicts()
+    all_data = [x for x in query_object]
+    return render_template('index.html', seasons=all_data)
 
 
-@app.route('/<game_id>', methods=['GET'])
+@app.route("/<int:season>")
+def get_all_games_in_season(season):
+    query_object = Nflpbp.select(Nflpbp.gameid,Nflpbp.game_date,Nflpbp.hometeam,Nflpbp.awayteam).distinct().where(Nflpbp.season==season).dicts()
+    all_games = [x for x in query_object]
+    return render_template('all_games_season.html', all_games=all_games, season=season)
+
+@app.route('/game/<game_id>/playbyplay')
+def get_index(game_id):
+    return render_template('playbyplay.html', gameid=game_id)
+
+
+@app.route('/game/<game_id>', methods=['GET'])
 def get_game_data(game_id):
-    #query_object = Nflpbp.select(Nflpbp.gameid,Nflpbp.qtr,Nflpbp.timesecs,Nflpbp.playtype,Nflpbp.desc).where(Nflpbp.gameid==game_id).dicts()
+    #query_object = Nflpbp.select(Nflpbp.gameid,Nflpbp.qtr,Nflpbp.timesecs,Nflpbp.playtype,Nflpbp.desc,Nflpbp.yards_gained,Nflpbp.yrdln,Nflpbp.ydstogo,Nflpbp.yrdline100,Nflpbp.awayteam).where(Nflpbp.gameid==game_id).dicts()
     #all_data = [x for x in query_object]
     query_object = Nflpbp.select().where(Nflpbp.gameid==game_id)
     all_data = [x.to_dict() for x in query_object]
     
     data = {}
-    #import pdb
-    #pdb.set_trace()
     for item in all_data:
         qtr = item['qtr']
         item['yards_gained'] = float(item['yards_gained'])
         item['yrdln'] = float(item['yrdln'])
         item['ydstogo'] = float(item['ydstogo'])
+        item['yrdline100'] = float(item['yrdline100'])
         if qtr not in data:
             data[qtr] = []
         data[qtr].append(item)
--- static/js/base.js
+++ static/js/base.js
@@ -1,60 +1,70 @@
 $(document).ready(function(){
 	var first_quarter_data;
 	var current_play_index = 0;
-	var play_timeout = 3000;
+	var play_timeout = 2000;
 
 	function set_game_state_for_play(play_data) {
 		$("#play_description").html(play_data.desc);
-		$("#football_pos").css({left: posteam_yrds(play_data.posteam, play_data.yrdln, play_data.yards_gained, play_data.sideoffield)});
-		console.log(play_data.yrdln, play_data.yards_gained)
-		var calc_left =  firstdown_line(play_data.posteam, play_data.yrdln, play_data.ydstogo);
+		$("#football_pos").css({left: posteam_yrds(play_data.awayteam, play_data.posteam, play_data.yrdln, play_data.yards_gained, play_data.yrdline100)});
+		var calc_left =  firstdown_line(play_data.awayteam,play_data.posteam, play_data.yrdline100, play_data.ydstogo);
 		$("#first_down_line").css({left: calc_left});
+		
+		$(".quarter_number").html(play_data.qtr);
+		$(".time_left").html(play_data.game_time);
+		$(".current_down").html(play_data.down);
+		$(".yards_to_go").html(play_data.ydstogo);
+		$(".team1").html(play_data.posteam);
+		$(".team1_score").html(play_data.posteamscore);
+		$(".team2").html(play_data.defensiveteam);
+		$(".team2_score").html(play_data.defteamscore);
+		$(".side_of_field").html(play_data.sideoffield);
+		$(".yard_line").html(play_data.yrdln);
 
 	}
 
 	function yrds_to_pxl(in_yards){
-		return in_yards*11.3;
+		return in_yards*9.75;
 	}
 
-	function posteam_yrds(posteam, yrdln, yrds_gained, sideoffield){
-		if (posteam == "NE" && (yrdln+yrds_gained)<50) {
-			return ((yrds_to_pxl(yrdln+yrds_gained) + 95) + "px");
+	function posteam_yrds(awayteam, posteam, yrdln, yrds_gained, yrdline100){
+		if (posteam == awayteam) {
+			return ((yrds_to_pxl(yrdline100-yrds_gained) + 95) + "px");
 		}
-		else if (posteam == "NE" && (yrdln+yrds_gained)>50 && posteam !== sideoffield){
-			return ((yrds_to_pxl(100-(yrdln+yrds_gained))) + 95 + "px")
-		}
-		else if (posteam == "PIT" && (yrdln+yrds_gained)<50){
-			return ((1073 - yrds_to_pxl(yrdln+yrds_gained)) + "px");
-		}
-		else if (posteam == "PIT" && (yrdln+yrds_gained)>50 && posteam == sideoffield){
-			return ((1073 - yrds_to_pxl(100-(yrdln+yrds_gained))) + "px");
+		else if (posteam !== awayteam){
+			return ((1073 - yrds_to_pxl(yrdline100-yrds_gained)) + "px");
 		}
 	}
 
-	function firstdown_line(posteam, yrdln, yrds_to_go){
-		if (posteam == "PIT") {
-			return ((yrds_to_pxl(yrdln+yrds_to_go) + 95) + "px");
+	function firstdown_line(awayteam, posteam, yrdline100, yrds_to_go){
+		if (posteam == awayteam) {
+			return ((yrds_to_pxl(yrdline100-yrds_to_go) + 95) + "px");
 		}
-		else if (posteam == "NE"){
-			return ((1073 - yrds_to_pxl(yrdln+yrds_to_go)) + "px");
+		else if (posteam !== awayteam){
+			return ((1073 - yrds_to_pxl(yrdline100-yrds_to_go)) + "px");
 		}	 
 	}
 
 	function perform_next_play() {
 		current_play = first_quarter_data[current_play_index];
-		if (current_play.playtype !== 'Kickoff' || current_play.playtype !== 'Punt' || current_play.playtype !== 'Field Goal'){
+		if (current_play.playtype == 'Kickoff' || current_play.playtype == 'Punt' || current_play.playtype == 'Field Goal' || current_play.playtype == 'Timeout'){
+
+		}
+		else{
 			set_game_state_for_play(current_play);
+		}
     	if (first_quarter_data.length == current_play_index + 1) {
 
-    	} else {
+    	} 
+    	else {
     		current_play_index += 1;
     		setTimeout(perform_next_play, play_timeout);
     	}
-		}
 	}
 
-	$.get("/2013110309", function(data){
+	var url = $("#game_data_url").val();
+	$.get(url, function(data){
+	  $("#loading").hide();
 	  first_quarter_data = data[1];
-	  setTimeout(perform_next_play, play_timeout);
+	  perform_next_play();
 	});
 });
